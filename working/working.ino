e8ecb51753872c0436ad04be0fd59b6dbb1effbb
ktomash <katomashe@gmail.com>
2018-08-23 14:17:17 -0700

    remove unused code

--- working/working.ino
+++ working/working.ino
@@ -14,8 +14,6 @@ int l_IBI = 0;
 void setup() {
   Serial.begin(115200);
   Serial.println("TEST");
-//  pinMode(13, OUTPUT);
-//  Serial.write('1');
   strip.begin();
   strip.show();
 }
@@ -43,55 +41,6 @@ void loop() {
       }
     }
     l_IBI = IBI; 
-   /* for (int i = 0; i <= IBI; i++) {
-      strip.setPixelColor(i, strip.Color(255, 0, 0));  
-      strip.show();
-    }
-    for (int j = IBI; j <= 60; j++) {
-      strip.setPixelColor(j, strip.Color(0, 0, 0));  
-      strip.show(); 
-    } */
-
-   
-   // colorSet(30, strip.Color(IBI, 0, 0));
-   /* if (IBI > 0){
-      //for (int j = 0; j < IBI; j++) {
-        
-        strip.setPixelColor(IBI, strip.Color(0, 0, 255));
-        strip.show();
-        delay(100); 
-    //} 
-    } */
  }      
 }
-    
-    
-    
-/*    digitalWrite(13, HIGH);
-    delay(500);
-    digitalWrite(13, LOW);
-    delay(500);
-    digitalWrite(13, HIGH);
-    delay(500);
-    digitalWrite(13, LOW);
-    Serial.write('0'); */
 
-/*
-void colorSet(uint32_t c) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-  }
-} */
-/*
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    int mr_IBI = (int)Serial.read();
-    // add it to the inputString:
-    int IBI = mr_IBI;
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
-  }
-}
-*/
