11940c7ef9070f7f723d8cc4a38ae899c0191c1d
ktomash <katomashe@gmail.com>
2018-08-24 16:24:49 -0700

    better

--- working/working.ino
+++ working/working.ino
@@ -8,10 +8,6 @@
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 
-int l_CS = 7;
-int oR = 0;
-int oG = 0;
-int oB = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -27,13 +23,40 @@ void loop() {
   if (Serial.available() > 0) {
     int CS = Serial.read();
 
-    for (int j = 0; j < strip.numPixels(); j++) {
+  /*  for (int j = 0; j < strip.numPixels(); j++) {
       strip.setPixelColor(j, strip.Color((200-2*CS*CS), (200-2*(CS-10)*(CS-10)), (255-(CS-30)*(CS-30))));
       strip.show(); 
+    } */
+    if (CS < 6) {
+      for (int j = 0; j < strip.numPixels(); j++) {
+        strip.setPixelColor(j, strip.Color(255, 0, 0));
+        strip.show();
+        delay(5); 
+      }
     }
 
+    if (CS < 13 && CS >=6) {
+      for (int j = 0; j < strip.numPixels(); j++) {
+        strip.setPixelColor(j, strip.Color(255-(CS-6)*35, 0, (CS-6)*35));
+        strip.show(); 
+        delay(5); 
+      }
+    }
+    if (CS >= 13 && CS < 25) {
+      for (int j = 0; j < strip.numPixels(); j++) {
+        strip.setPixelColor(j, strip.Color(0, 20*(CS-13), 255-(20*(CS-13))));
+        strip.show(); 
+        delay(5); 
+      }
+    }
+    if (CS >= 25) {
+      for (int j = 0; j < strip.numPixels(); j++) {
+        strip.setPixelColor(j, strip.Color(0, 255, 0));
+        strip.show(); 
+        delay(5); 
+      }
+    }
 
-    l_CS = CS;
   }      
 }
 
